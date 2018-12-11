7879a6eabf396dab84766c55527dea0d75728695
Rasmus Zweidorff Iversen <rasmus.z.iversen@gmail.com>
2018-12-11 22:17:25 +0100

    Added AppModule

--- src/main/app/di/modules/AppModule.ts
+++ src/main/app/di/modules/AppModule.ts
@@ -1,3 +1,26 @@
-export class AppModule implements IModule {
+import { Subject } from "rxjs";
+import App from "../../App";
+import { IModule } from "../../interfaces/IModule";
+import { ActionSupplier } from "../suppliers/ActionSupplier";
+import { AppSupplier } from "../suppliers/AppSupplier";
+import { EndpointSupplier } from "../suppliers/EndpointSupplier";
+import { ResponseSupplier } from "../suppliers/ResponseSupplier";
+import { ScenarioSupplier } from "../suppliers/ScenarioSupplier";
+import { StateSupplier } from "../suppliers/StateSupplier";
+
+export class AppModule {
+
+    constructor(private _activeStateSubject: Subject<string>) {}
+
+    public build(json: any): App {
+        const actionSupplier = new ActionSupplier(this._activeStateSubject);
+        const responseSupplier = new ResponseSupplier();
+        const endpointSupplier = new EndpointSupplier(responseSupplier, actionSupplier);
+        const stateSupplier = new StateSupplier(endpointSupplier);
+        const scenarioSupplier = new ScenarioSupplier(stateSupplier);
+        const appSupplier = new AppSupplier(scenarioSupplier);
+
+        return appSupplier.createFromJSON(json);
+    }
 
 }
