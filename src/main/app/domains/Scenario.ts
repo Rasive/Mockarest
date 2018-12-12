a356b6e28b204bd7d5d38a8c915e3e20d139a56e
Rasmus Zweidorff Iversen <rasmus.z.iversen@gmail.com>
2018-12-12 20:41:06 +0100

    Changed Scenario::states to be public

--- src/main/app/domains/Scenario.ts
+++ src/main/app/domains/Scenario.ts
@@ -8,18 +8,12 @@ export class Scenario {
     public name: string;
     public description: string;
 
-    private _states: State[];
+    public states: State[];
+
+    constructor(public activeStateSubject: Subject<string>) { }
 
     public findState(id: string): State {
-        return this._states.find((obj: State) => obj.id === id);
-    }
-
-    public get states(): State[] {
-        return this._states;
-    }
-
-    public set states(states: State[]) {
-        this._states = states;
+        return this.states.find((obj: State) => obj.id === id);
     }
 
 }
