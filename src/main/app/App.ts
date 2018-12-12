5ef0ade255f3b076cf36d01647e0e95b057a8c15
Rasmus Zweidorff Iversen <rasmus.z.iversen@gmail.com>
2018-12-12 23:00:06 +0100

    Added subscriptions to active router in App

--- src/main/app/App.ts
+++ src/main/app/App.ts
@@ -1,22 +1,23 @@
 import * as Express from "express";
-import { Subject } from "rxjs";
+import { Router } from "express";
+import { Observable, Subject } from "rxjs";
 import { Scenario } from "./domains/Scenario";
 import { Log } from "./utils/Log";
 
 export default class App {
 
-    private _router: Express.Router;
-
-    public scenario: Scenario;
-
     constructor(
         private readonly _port: number,
         private readonly _app: Express.Application,
-        public readonly routerSubject: Subject<Express.Router>) {
-            this.routerSubject.subscribe((router) => {
-                this._router = router;
-            });
-        }
+        public readonly activeRouterObservable: Observable<Router>) {
+        this.activeRouterObservable.subscribe((router) => {
+            this._router = router;
+        });
+    }
+
+    private _router: Express.Router;
+
+    public scenario: Scenario;
 
     public run(): any {
         this._app.use((req, res, next) => {
