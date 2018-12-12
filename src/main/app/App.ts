bb77a0dc1b88f5255d4cb5ff9c0a5371cc2b502e
Rasmus Zweidorff Iversen <rasmus.z.iversen@gmail.com>
2018-12-12 21:50:13 +0100

    Made App use Express routing

--- src/main/app/App.ts
+++ src/main/app/App.ts
@@ -1,12 +1,30 @@
 import * as Express from "express";
 import minimist = require("minimist");
 import { Scenario } from "./domains/Scenario";
+import { Log } from "./utils/Log";
 
 export default class App {
 
+    public express;
+    public router: Express.Router;
     public scenario: Scenario;
 
-    public run(): any {
-        throw new Error("Method not implemented.");
+    constructor(private readonly _port: number) {
+        this.express = Express();
     }
+
+    public run(): any {
+        this.express.use((req, res, next) => {
+            if (this.router) {
+                this.router(req, res, next);
+            } else {
+                next();
+            }
+        });
+
+        this.express.listen(this._port, () => {
+            Log.verbose("Server started on port: " + this._port);
+        });
+    }
+
 }
