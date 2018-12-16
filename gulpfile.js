aa820e287dd123277c2f80d7e62e61ad2ab02dda
Rasmus Zweidorff Iversen <rasmus.z.iversen@gmail.com>
2018-12-16 12:52:18 +0100

    Messing around with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+var typescript = require("typescript");
 var upath = require("upath");
-var print = require("gulp-print").default;
+var tjs = require("typescript-json-schema");
 var tap = require("gulp-tap");
+var fs = require("fs");
 
-gulp.task("build-dist", () => {
+gulp.task("build:dist:dev", () => {
     var tsProject = ts.createProject("tsconfig.json")
 
     return gulp.src("./src/main/**/*.ts")
@@ -16,4 +18,25 @@ gulp.task("build-dist", () => {
             file.contents = Buffer.from(String(file.contents).replace(/"@app(\/.+)"/g, `"./${upath.relative(normalizedFileDirAbsolutePath, normalizedAppAbsolutePath + "$1")}"`));
         }))
         .pipe(gulp.dest("dist"))
+});
+
+gulp.task("gen:schema", (done) => {
+    var basePath = "./src/main";
+    var compilerOptions = {
+        "target": "es6",
+        "module": "commonjs",
+        "moduleResolution": "node",
+        "experimentalDecorators": true,
+        "baseUrl": "./src",
+        "paths": {
+            "@app/*": [
+                "main/app/*"
+            ],
+        }
+    };
+    var settings = {};
+    var program = tjs.getProgramFromFiles(["./src/main/app/interfaces/index.ts"], compilerOptions);
+    var schema = tjs.generateSchema(program, "IScenario", settings);
+
+    fs.writeFile("./res/schemas/schema.json", JSON.stringify(schema, null, 4), null, done);
 });
