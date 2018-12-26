922acafafea4997991827d2531e0acbf9325d7c3
Rasmus Zweidorff Iversen <rasmus.z.iversen@gmail.com>
2018-12-26 19:39:11 +0100

    Fixing Scenario tests

--- src/test/unit/domains/Scenario.spec.ts
+++ src/test/unit/domains/Scenario.spec.ts
@@ -1,27 +1,26 @@
-import { Mock } from "typemoq";
-import { State, Scenario } from "@app/domains";
-import { expect } from "chai";
+import { Scenario, State } from "@app/domains";
 import { IState } from "@app/interfaces";
-import { Substitute } from "@fluffy-spoon/substitute";
+import { expect } from "chai";
+import { instance, mock, when } from "ts-mockito";
 
 describe("Scenario", () => {
     context("findState", () => {
         it("should find state from state id", () => {
-            // arrange
-            const stateMock1 = Substitute.for<IState>()
-            stateMock1.id.returns("one");
-            const stateMock2 = Substitute.for<IState>();
-            stateMock2.id.returns("two");
-            const stateMock3 = Substitute.for<IState>();
-            stateMock3.id.returns("three");
+            // given
+            const stateMock1 = mock(State);
+            when(stateMock1.id).thenReturn("one");
+            const stateMock2 = mock(State);
+            when(stateMock2.id).thenReturn("two");
+            const stateMock3 = mock(State);
+            when(stateMock3.id).thenReturn("three");
 
             const scenario = new Scenario(null, null);
-            scenario.states = [stateMock1, stateMock2, stateMock3];
+            scenario.states = [instance(stateMock1), instance(stateMock2), instance(stateMock3)];
 
-            // act
+            // when
             const state = scenario.findState("two");
-            
-            // assert
+
+            // then
             expect(state).to.be.not.undefined;
         });
     });
